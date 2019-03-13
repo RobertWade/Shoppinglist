8cee65cab2ec807fdabcbb9b0acd36620fb11a7a
Tutorials <Tutorials@Roberts-MacBook-Pro.local>
2019-03-13 08:38:18 +0100

    Part 1: Added Veutify and basic functionality to add and remove items from a list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-list>
+      <v-list-tile v-for="(item,i) in items" :key="i" avatar>
+        <v-list-tile-content>{{i}}. {{item}}</v-list-tile-content>
+        <v-list-tile-action>
+          <v-icon @click="remove(i)">delete</v-icon>
+        </v-list-tile-action>
+      </v-list-tile>
+      <v-text-field
+        v-model="newItem"
+        @click:append="addItem"
+        solo
+        label="Add item"
+        append-icon="add"
+      ></v-text-field>
+    </v-list>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  data: () => ({
+    items: ["Bananas", "Spinach", "Icecream"],
+    newItem: ""
+  }),
+  methods: {
+    remove(index) {
+      const list = this.items;
+      const newList = [
+        ...list.slice(0, index),
+        ...list.slice(index + 1, list.length)
+      ];
+      this.items = newList;
+    },
+    addItem() {
+      const list = this.items;
+      const newList = [...list, this.newItem];
+      this.items = newList;
+    }
   }
-}
+};
 </script>
+
+<style lang="scss" scoped>
+.home {
+  padding: 20px;
+}
+</style>
